df82e6469cf54257a67bf9910a55fa2977e2b549
lilygingery <122523543+lilygingery@users.noreply.github.com>
2025-11-10 17:09:41 -0800

    bump version from 1.1.6 to 1.1.7

--- batch_archived_or_not.py
+++ batch_archived_or_not.py
@@ -13,7 +13,7 @@ from PySide6.QtWidgets import (QApplication, QTextEdit, QWidget, QVBoxLayout, QP
 from PySide6.QtCore import Qt
 from creds import APP_API_USERNAME, APP_API_PASSWORD
 
-VERSION = "1.1.6"
+VERSION = "1.1.7"
 URL_TEMPLATE = r"https://{}/api/archived_or_not"
 # ADDRESS = r"localhost:5000" # for testing
 ADDRESS = r"ppdo-prod-app-1.vm.aws.ucsc.edu"
